b4338f549ea0ba13dac375d742097f91e2286020
Simon Berger <simon.berger95@gmail.com>
2025-02-06 13:21:01 +0100

    Add vimtex

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -32,4 +32,11 @@ return {
         dependencies = { 'stevearc/conform.nvim', 'williamboman/mason.nvim' },
         opts = {},
     },
+    {
+        'lervag/vimtex',
+        lazy = false, -- we don't want to lazy load VimTeX
+        init = function()
+            vim.g.vimtex_view_method = 'zathura'
+        end,
+    },
 }
