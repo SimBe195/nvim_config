4434c6c98f657d5c5c4b486472ed7fa4f913c398
Simon Berger <simon.berger95@gmail.com>
2025-06-20 12:51:13 +0200

    Use osc52 for clipboard

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -46,4 +46,24 @@ opt.fillchars = {
     eob = ' ',
 }
 
+-- Clipboard
+if vim.env.SSH_TTY then
+    opt.clipboard:append { 'unnamed', 'unnamedplus' }
+    local function paste()
+        return { vim.fn.split(vim.fn.getreg '', '\n'), vim.fn.getregtype '' }
+    end
+    local osc52 = require 'vim.ui.clipboard.osc52'
+    vim.g.clipboard = {
+        name = 'OSC 52',
+        copy = {
+            ['+'] = osc52.copy '+',
+            ['*'] = osc52.copy '*',
+        },
+        paste = {
+            ['+'] = paste,
+            ['*'] = paste,
+        },
+    }
+end
+
 -- vim: ts=2 sts=2 sw=2 et
